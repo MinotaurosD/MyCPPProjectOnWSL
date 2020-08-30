b71f3707970c5782b6f1329c226411ddf0a10d9c
MinotaurosD <doraemon1970@163.com>
2020-08-31 00:59:16 +0800

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -15,18 +15,6 @@ int main()
         cout << word << " ";
     }
     cout << endl;
-
-    int data1[] = {1, 2, 3, 4, 5};
-    int size1 = sizeof(data1); //求数组的大小
-
-    int *data2 = data1;
-    int size2 = sizeof(data2); //
-
-    int size3 = GetSize(data1);
-
-    cout << size1 << endl
-         << size2 << endl
-         << size3 << endl;
 }
 
 int GetSize(int data[])
